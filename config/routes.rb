e4b879c0d9e27eb7b2217749fbedabf9e1463ad1
mhope21 <juddhope@gmail.com>
2024-07-23 10:39:00 -0500

    Lesson 6 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  # resources :users
+  post '/users/:id/logon', to: 'users#logon', as: 'user_logon'
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  post '/users', to: 'users#create'
+  patch '/users/:id', to: 'users#update'
+  delete '/users/logoff', to: 'users#logoff', as: 'user_logoff'
+  delete '/users/:id', to: 'users#delete'
+
+  
+  resources :forums
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
